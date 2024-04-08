c2b411da695bad828c02048d186f9325752f8f39
Aniketh <anikethpala@gmail.com>
2024-04-08 17:19:39 +0530

    test2

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
-session_start();
 $databaseHost = 'tbf-db.chwiu8qak8lg.eu-north-1.rds.amazonaws.com';
-$databaseUser = 'admin'; 
-$databasePassword = 'Aniketh2204'; // Removed space
+$databasePort = '3306'; // Change this to the port your MySQL server is using
+$databaseUser = 'admin';
+$databasePassword = 'Aniketh2204';
 $databaseName = 'test';
 
 try {
-  $conn = new PDO("mysql:host=$databaseHost;dbname=$databaseName", $databaseUser, $databasePassword);
+  $conn = new PDO("mysql:host=$databaseHost;port=$databasePort;dbname=$databaseName", $databaseUser, $databasePassword);
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $error) {
   echo "Something went wrong: " . $error->getMessage();
